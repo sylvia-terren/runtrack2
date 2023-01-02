57363365e1ed64ca3b2f6afd6959a45089575fe6
Sylvia Terren <sylvia.terren@laplateforme.io>
2023-01-02 23:07:01 +0100

    Manipulation de la variable $val et la variable $myBool

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -11,7 +11,13 @@
         $str = "LaPlateforme";
         $str2 = "Vive";
         $str3 = "!";
-        echo "$str2 $str $str3";
+        $val = 6;
+        $myBool = false;
+        echo "$str2 $str $str3 \n";
+        echo $val + 4, "\n";
+        echo $myBool;
+
+
     ?>
 </body>
 </html>
