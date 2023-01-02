393e790dbb6a283bc3c90ff50741b076a71abb8d
Sylvia Terren <sylvia.terren@laplateforme.io>
2023-01-02 23:43:18 +0100

    Finition du job03 et modification du index.php dans job01

---
+++ jour01/job03/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $bolean = true; //booléen
+    $str = "patate"; //chaine de caractères
+    $str2 = 'une patate'; //chaine de caractères 2
+    $int = 23; //entier
+    $x = 8.5; //nombre virugle flottant
+    //define a is_primitive_type() pour tester
+//type nom valeur
+    echo "
+    <table>
+        <thread>
+            <th>Type</th>
+            <th>Nom</th>
+            <th>Valeur</th>
+        <thead>
+            <tbody>
+                <tr>
+                    <td>Variable</td>
+                    <td>Booléen</td>
+                    <td>$bolean</td>
+                </tr>
+                <tr>
+                    <td>Variable</td>
+                    <td>chaine de caractères</td>
+                    <td>$str</td>
+                </tr>
+                <tr>
+                    <td>Variable</td>
+                    <td>chaine de caractères vers.2</td>
+                    <td>$str2</td>
+                </tr>
+                <tr>
+                    <td>Variable</td>
+                    <td>Entier</td>
+                    <td>$int</td>
+                </tr>
+                <tr>
+                    <td>Variable</td>
+                    <td>Entier flottant décimal</td>
+                    <td>$x</td>
+                </tr>
+            </tbody>
+    </table>
+    ";
+    ?>
+</body>
+</html>
