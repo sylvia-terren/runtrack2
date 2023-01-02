85ab3973103ab49cf31bfe043b4b48884e8758ba
Sylvia Terren <sylvia.terren@laplateforme.io>
2023-01-02 23:15:44 +0100

    Création des dossiers job02 et job03, affichage des variable dans index.php de job02 avec mise en commentaire de trois manières différentes

---
+++ jour01/job02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        /*$hello = "Hello La Plateforme!";
+        $str1 = "Hello";
+        $str2 = "La Plateforme!";*/
+        #echo "$str1 $str2 \n";
+        //print $hello;
+    ?>
+</body>
+</html>
